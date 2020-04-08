0276f20890474fa65afb209e9f7f8cc5b0264b45
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-08 13:29:05 -0700

    instantiates new player

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -37,7 +37,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player('Daaaaaaaaan', room['outside'])
 # Write a loop that:
 action = True
 while action != 'q':
