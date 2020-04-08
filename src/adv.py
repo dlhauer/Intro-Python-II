444c6494c65025632d68b0f8c524b290b3bdc75a
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-08 15:36:36 -0700

    adds items to rooms, prints items

--- src/adv.py
+++ src/adv.py
@@ -4,21 +4,21 @@ from textwrap import wrap
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons."),
+                     "North of you, the cave mount beckons.", ['map', 'suit of armor']),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ['candelabra', 'quill and ink', 'parchment']),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ['rope']),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ['sword', 'rusty keys', 'wax-sealed envelope']),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ['compass']),
 }
 
 room['outside'].n_to = room['foyer']
@@ -38,8 +38,11 @@ while True:
     print(
         f'\nCurrent room: {player.current_room.name}.\n'
         f'{player.current_room.description}\n'
+        'Available items:'
     )
-    action = input("""What would you like to do?\n
+    for item in player.current_room.items:
+        print(item)
+    action = input("""\nWhat would you like to do?\n
     n - move north\n
     s - move south\n
     e - move east\n
