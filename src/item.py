9b4324240fad06084d7fdb84719788512bc135d6
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-09 10:41:39 -0700

    adds LightSource class

--- src/item.py
+++ src/item.py
@@ -8,3 +8,12 @@ class Item:
 
     def on_drop(self):
         print(f'You have dropped the {self.name}!')
+
+
+class LightSource(Item):
+    def __init__(self, name, description):
+        super().__init__(name, description)
+
+    def on_drop(self):
+        print(
+            f'Your {self.name} is a light source. It is unwise to drop it.')
